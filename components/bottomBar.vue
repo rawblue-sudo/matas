50034335aaa27c82fbde173ef1cb082b4897bda4
rawblue-sudo <56031736+rawblue-sudo@users.noreply.github.com>
2020-06-18 14:39:48 +0200

    initial app upload

---
+++ components/bottomBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <footer class="footer">
+      <img src="logo_final_white.png" alt="Matas Pizza & Tagliere" />
+      <p>
+        Via Nicola Matas 54 <br class="desktop" />Ancona <br />Tel
+        <a href="tel:0719943040">0719943040</a>
+      </p>
+      <a
+        class="mobile"
+        href="https://www.instagram.com/matas54pizzeria/"
+        target="_blank"
+        rel="noopener"
+        aria-label="Matas54 su Instagram"
+      >
+        <i class="fab fa-instagram mobile" aria-hidden="true"></i
+      ></a>
+      <a
+        class="mobile"
+        href="https://www.facebook.com/matas54pizzeria/"
+        target="_blank"
+        rel="noopener"
+        aria-label="Matas54 su Facebook"
+      >
+        <i class="fab fa-facebook-f mobile" aria-hidden="true"></i
+      ></a>
+      <nuxt />
+    </footer>
+    <div class="footerdiv"></div>
+  </div>
+</template>
